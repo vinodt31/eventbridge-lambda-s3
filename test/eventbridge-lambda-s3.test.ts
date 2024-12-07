f498918df5a8d5eaead0d8a46b4a252688451333
vinodt31 <vinodt31@gmail.com>
2024-12-07 22:38:45 +0530

    Initial commit

---
+++ test/eventbridge-lambda-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventbridgeLambdaS3 from '../lib/eventbridge-lambda-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eventbridge-lambda-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventbridgeLambdaS3.EventbridgeLambdaS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
